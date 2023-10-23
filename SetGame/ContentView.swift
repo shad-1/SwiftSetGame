544601a0c6a9f4a03374a0dd2c11572f2cc13f16
shad <shadb@corebridge.net>
2023-10-23 17:59:52 -0600

    Initial Commit

---
+++ SetGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SetGame
+//
+//  Created by Shad Baird on 10/23/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by Shad Baird on 10/23/23.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
